4079f51e57a784e7f51619c952dae2b61eac3dd4
rafa-anholeto <rafaanholeto3@gmail.com>
2022-03-24 14:00:40 -0300

    API Parking Spot

---
+++ parking-control/src/main/java/com/api/parkingcontrol/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.api.parkingcontrol.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException resourceNotFoundException, HttpServletRequest request){
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, resourceNotFoundException.getMessage(), request.getRequestURI());
+        return new ResponseEntity<>(err, status);
+    }
+}
